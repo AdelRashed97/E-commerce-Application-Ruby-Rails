780ec92c418769adcb0f6d2c60334add881fd79f
Adel Rashed <adel_rashed@outlook.com>
2020-07-28 17:03:26 +0000

    lower case email before saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    raise user_params.inspect
     user = User.new(user_params)
     if user.save
       session[:user_id]=user.id
@@ -14,6 +15,8 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation)
+    permitted = params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation)
+    permitted[:email] = permitted[:email].strip.downcase
+    return permitted
   end
 end
