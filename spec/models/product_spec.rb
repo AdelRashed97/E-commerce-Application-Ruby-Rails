0dbbab436f30eaf1d345c01d688bd6c1e7bce1ff
Adel Rashed <adel_rashed@outlook.com>
2020-07-28 13:07:11 +0000

    create a Category for each test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    before(:all) do
-     Category.destroy_all
-     Category.create( name: 'Apparel')
-    end
+
 
     context "Given all parameters" do
       it "saves" do
-        product = Product.create(name:'name',description:'description',image:"image",quantity: 1,price: 5,category_id:1)
+        category=Category.create( name: 'Apparel')
+        product = Product.create(name:'name',description:'description',image:"image",quantity: 1,price: 5,category_id:category.id)
         expect(product.errors.messages).to be {}
 
       end
@@ -17,7 +15,8 @@ RSpec.describe Product, type: :model do
 
     context "Missing a product name " do
       it "give an error" do
-        product = Product.create(description:'description',image:"image",quantity: 1,price: 5,category_id:1)
+        category=Category.create( name: 'Apparel')
+        product = Product.create(description:'description',image:"image",quantity: 1,price: 5,category_id:category.id)
         expect(product.errors[:name]).not_to be_empty
 
        end
@@ -26,7 +25,8 @@ RSpec.describe Product, type: :model do
 
     context "Missing a product price" do
       it "give an error" do
-        product = Product.create(name:'name',description:'description',image:"image",quantity: 1,category_id:1)
+        category=Category.create( name: 'Apparel')
+        product = Product.create(name:'name',description:'description',image:"image",quantity: 1,category_id:category.id)
         expect(product.errors[:price]).not_to be_empty
 
        end
@@ -35,7 +35,8 @@ RSpec.describe Product, type: :model do
 
     context "Missing a product quantity" do
       it "give an error" do
-        product = Product.create(name:'name',description:'description',image:"image",price: 1,category_id:1)
+        category=Category.create( name: 'Apparel')
+        product = Product.create(name:'name',description:'description',image:"image",price: 1,category_id:category.id)
         expect(product.errors[:quantity]).not_to be_empty
 
        end
