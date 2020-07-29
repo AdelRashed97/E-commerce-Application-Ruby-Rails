84f03dcc1a22b7dc29e5b5dfb70ebea55e9e2cd0
Adel Rashed <adel_rashed@outlook.com>
2020-07-29 19:48:29 +0000

    test that adding to the cart increases that number of items in the cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Add to cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "number of items in cart increase" do
+    # ACT
+    visit root_path
+    expect(page).to have_content("My Cart (0)")
+    first('.product').click_button('Add')
+    # DEBUG
+    
+    # VERIFY
+    expect(page).to have_content("My Cart (1)")
+    save_screenshot
+  end
+end
